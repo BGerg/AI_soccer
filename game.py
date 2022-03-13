7cf0e4b8f4982b2f00f42428185177e1c0de456e
Your Name <you@example.com>
2022-03-13 11:55:03 +0100

    players and gate add

--- game.py
+++ game.py
@@ -21,7 +21,7 @@ SCREEN_WIDTH  = int(1.869*SCREEN_HEIGHT)
 # Define a player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
 class Player(pygame.sprite.Sprite):
-    def __init__(self,player_color):
+    def __init__(self,player_color, player_posx, player_posy):
         super(Player, self).__init__()
         self.player_color = player_color
         if self.player_color == "blue":
@@ -29,7 +29,7 @@ class Player(pygame.sprite.Sprite):
         elif self.player_color == "yellow":
             self.surf = pygame.image.load("player_yellow.png").convert()
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.surf.get_rect()
+        self.rect = pygame.Rect(player_posx, player_posy, 44, 77)
         self.ball = False
 
 
@@ -67,7 +67,6 @@ class Player(pygame.sprite.Sprite):
             self.surf = pygame.image.load("player_yellow.png").convert()
         elif self.player_color == "blue" and self.ball == False:
             self.surf = pygame.image.load("player_blue.png").convert()
-            print("dsfdsf")
         elif self.player_color == "yellow" and self.ball:
             self.surf = pygame.image.load("player_yellow_w_ball.png").convert()
         elif self.player_color == "blue" and self.ball:
@@ -92,6 +91,19 @@ class Ball(pygame.sprite.Sprite):
     def update(self, positions):
         self.rect.move_ip(positions)
 
+class SoccerGateBlue(pygame.sprite.Sprite):
+    def __init__(self):
+        super(SoccerGateBlue, self).__init__()
+        self.surf = pygame.image.load("soccergate.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = pygame.Rect(SCREEN_WIDTH-70, SCREEN_HEIGHT/2-80, 50, 150)
+
+class SoccerGateYellow(pygame.sprite.Sprite):
+    def __init__(self):
+        super(SoccerGateYellow, self).__init__()
+        self.surf = pygame.image.load("soccergate.png").convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = pygame.Rect(0, SCREEN_HEIGHT/2-80, 50, 150)
 
 # Setup for sounds. Defaults are good.
 #pygame.mixer.init()
@@ -108,10 +120,23 @@ ADDBALL = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDBALL, 250)
 
 # Instantiate player. Right now, this is just a rectangle.
-player_yellow = Player("yellow")
-player_blue = Player("blue")
-new_ball = Ball()
+yellow_one = Player("yellow", 440, 240)
+yellow_two = Player("yellow", 390, 375)
+yellow_three = Player("yellow", 390, 95)
+yellow_four = Player("yellow", 255, 130)
+yellow_five = Player("yellow", 255, 330)
+yellow_goalkeeper = Player("yellow", 55, 240)
 
+blue_one = Player("blue", 615, 240)
+blue_two = Player("blue", 670, 100)
+blue_three = Player("blue", 670, 375)
+blue_four = Player("blue", 805, 130)
+blue_five = Player("blue", 805, 330)
+blue_goalkeeper = Player("blue", 1005, 240)
+
+new_ball = Ball()
+blue_gate = SoccerGateBlue()
+yellow_gate = SoccerGateYellow()
 # Create groups to hold enemy sprites and all sprites
 # - ball is used for collision detection and position updates
 # - all_sprites is used for rendering
@@ -120,15 +145,26 @@ yellow_team = pygame.sprite.Group()
 blue_team =  pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
-yellow_team.add(player_yellow)
-blue_team.add(player_blue)
+yellow_team.add(yellow_one)
+blue_team.add(blue_one)
 ball.add(new_ball)
 
 blocks_hit_list = [blue_team]
 
-all_sprites.add(new_ball)
-all_sprites.add(player_yellow)
-all_sprites.add(player_blue)
+#all_sprites.add(new_ball)
+all_sprites.add(yellow_one)
+all_sprites.add(yellow_two)
+all_sprites.add(yellow_three)
+all_sprites.add(yellow_four)
+all_sprites.add(yellow_five)
+all_sprites.add(yellow_goalkeeper)
+
+all_sprites.add(blue_one)
+all_sprites.add(blue_two)
+all_sprites.add(blue_three)
+all_sprites.add(blue_four)
+all_sprites.add(blue_five)
+all_sprites.add(blue_goalkeeper)
 # Setup the clock for a decent framerate
 clock = pygame.time.Clock()
 
@@ -165,46 +201,47 @@ while running:
     pressed_keys = pygame.key.get_pressed()
 
     # Update the player sprite based on user keypresses
-    player_yellow.update(pressed_keys)
+    yellow_one.update(pressed_keys)
     # Update ball position
     new_ball.update((0,0))
 
     screen.blit(background_image, [0, 0])
     # Draw the player on the screen
-    screen.blit(player_yellow.surf, player_yellow.rect)
-
+    screen.blit(yellow_one.surf, yellow_one.rect)
+    print(yellow_one.rect)
     # Draw all sprites
     for entity in all_sprites:
         screen.blit(entity.surf, entity.rect)
-
-    if pygame.sprite.collide_rect(player_yellow, player_blue):
-        if player_blue.ball:
-            player_yellow.ball = True
-            player_blue.ball = False
-            player_yellow.has_ball()
-            player_blue.has_ball()
-        elif player_yellow.ball:
-            player_blue.ball = True
-            player_yellow.ball = False
-            player_blue.has_ball()
-            player_yellow.has_ball()
-
+    screen.blit(new_ball.surf, new_ball.rect)
+    screen.blit(blue_gate.surf, blue_gate.rect)
+    screen.blit(yellow_gate.surf, yellow_gate.rect)
+    if pygame.sprite.collide_rect(yellow_one, blue_one):
+        if blue_one.ball:
+            yellow_one.ball = True
+            blue_one.ball = False
+            yellow_one.has_ball()
+            blue_one.has_ball()
+        elif yellow_one.ball:
+            blue_one.ball = True
+            yellow_one.ball = False
+            blue_one.has_ball()
+            yellow_one.has_ball()
 
     # Check if any ball have collided with the player
-    if pygame.sprite.spritecollideany(player_yellow, ball):
+    if pygame.sprite.spritecollideany(yellow_one, ball):
         # If so, then remove the player and stop the loop
-        player_yellow.ball = True
-        player_yellow.has_ball()
+        yellow_one.ball = True
+        yellow_one.has_ball()
         # # Stop any moving sounds and play the collision sound
         # move_up_sound.stop()
         # move_down_sound.stop()
         # collision_sound.play()
 
     # Check if any ball have collided with the player
-    if pygame.sprite.spritecollideany(player_blue, ball):
+    if pygame.sprite.spritecollideany(blue_one, ball):
         # If so, then remove the player and stop the loop
-        player_yellow.ball = True
-        player_yellow.has_ball()
+        yellow_one.ball = True
+        palyer_one.has_ball()
         # # Stop any moving sounds and play the collision sound
         # move_up_sound.stop()
         # move_down_sound.stop()
