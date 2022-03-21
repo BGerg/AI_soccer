f4d17ad4412052eb2c431f1e7594b7c90ebbeaa8
Your Name <you@example.com>
2022-03-21 17:20:36 +0100

    client calculates implemented

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 import json
 import socket
 import time
-
+from scipy.spatial import distance
 import pygame
 import random
 # Import pygame.locals for easier access to key coordinates
@@ -21,13 +21,14 @@ from pygame.locals import (
 # Define constants for the screen width and height
 SCREEN_HEIGHT = 587
 SCREEN_WIDTH = int(1.869*SCREEN_HEIGHT)
-
+all_data = {}
 # Define a player object by extending pygame.sprite.Sprite
 # The surface drawn on the screen is now an attribute of 'player'
 class Player(pygame.sprite.Sprite):
-    def __init__(self,color, player_posx, player_posy):
+    def __init__(self, name, color, player_posx, player_posy):
         super(Player, self).__init__()
         self.color = color
+        self.name = name
         if self.color == "blue":
             self.surf = pygame.image.load("images/player_blue.png").convert()
         elif self.color == "yellow":
@@ -128,7 +129,7 @@ class Ball(pygame.sprite.Sprite):
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
 
 class SoccerGate(pygame.sprite.Sprite):
-    def __init__(self, position_x, position_y):
+    def __init__(self,position_x, position_y):
         super(SoccerGate, self).__init__()
         self.surf = pygame.image.load("images/soccergate.png").convert()
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
@@ -158,114 +159,7 @@ class GoalAreaYellow(pygame.sprite.Sprite):
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = pygame.Rect(50, SCREEN_HEIGHT/2-325/2, 162, 325)
 
-def get_closest_player(team, player_w_ball):
-    closest_player = ""
-    tmp_distance = 0
-    distance = 999999999999999999999999999
-    for player in team:
-        if player != player_w_ball:
-            tmp_distance = abs(player_w_ball.rect[0]-player.rect[0])\
-                           +abs(player_w_ball.rect[1]-player.rect[1])
-        else:
-            continue
-        if tmp_distance < distance:
-            distance = tmp_distance
-            closest_player = player
 
-    return closest_player
-
-def get_random_other_player_his_team(team):
-    return random.choice(list(team))
-
-
-def handle_players_collision(player_w_ball, team_wo_ball, team_w_ball):
-    action_number = random.randint(0, 2)
-    for player_wo_ball in team_wo_ball:
-        if pygame.sprite.collide_rect(player_w_ball, player_wo_ball):
-            #if action_number == 0:
-             #   pass
-            if action_number == 1:
-                teammate = get_random_other_player_his_team(team_w_ball)
-                #closest_player = get_closest_player(team_w_ball, player_w_ball)
-                player_w_ball.ball = False
-                teammate.ball = True
-                teammate.has_ball()
-                player_w_ball.has_ball()
-            elif action_number == 2:
-                player_w_ball.ball = False
-                player_wo_ball.ball = True
-                player_wo_ball.has_ball()
-                player_w_ball.has_ball()
-            break
-
-def who_has_ball(all_players, yellow_team, blue_team, gates):
-    team_wo_ball = ""
-    team_w_ball = ""
-    player_w_ball = ""
-    for player in all_players:
-        if player.ball:
-            player_w_ball = player
-
-    if player_w_ball in yellow_team:
-        team_wo_ball = blue_team
-        team_w_ball = yellow_team
-    else:
-        team_wo_ball = yellow_team
-        team_w_ball = blue_team
-
-    return player_w_ball, team_wo_ball, team_w_ball
-
-def move_players_to_enemy_gate(b_team, y_team, b_gate, y_gate, team_ball):
-    for player in y_team:
-        if player in team_ball:
-            a = "1" + (len(str(abs(player.rect[0] - b_gate.rect[0])))-1)*"0"
-            b = "1" + (len(str(abs(player.rect[1] - b_gate.rect[1])))-1)*"0"
-            pos_x = (b_gate.rect[0]- player.rect[0]) / (int(a)+10)
-            pos_y = (b_gate.rect[1]- player.rect[1]) / (int(b)+10)
-            player.move_to((pos_x, pos_y))
-    for player in b_team:
-        if player in team_ball:
-            a = "1" + (len(str(abs(player.rect[0] - y_gate.rect[0])))-1)*"0"
-            b = "1" + (len(str(abs(player.rect[1] - y_gate.rect[1])))-1)*"0"
-            pos_x = (y_gate.rect[0]- player.rect[0]) / (int(a)+10)
-            pos_y = (y_gate.rect[1]- player.rect[1]) / (int(b)+10)
-            player.move_to((pos_x, pos_y))
-
-
-def check_goal(team_w_ball):
-    for player in team_w_ball:
-        if player.color == "yellow":
-            goal_area = goal_area_blue
-            goal_keeper = blue_goalkeeper
-            gate = blue_gate
-        elif player.color == "blue":
-            goal_area = goal_area_yellow
-            goal_keeper = yellow_goalkeeper
-            gate = yellow_gate
-        if pygame.sprite.collide_rect(player, goal_area) and player.ball:
-            n = random.randint(0, 1)
-            player.ball = False
-            player.has_ball()
-
-            if n == 0 and blue_gate.ball != True:
-                goal_keeper.ball = True
-                goal_keeper.has_ball()
-            elif n == 1 and goal_keeper.ball != True:
-                gate.has_ball(True)
-
-            reset()
-
-def catch_ball_first(player_w_ball):
-    if not player_w_ball:
-        n = random.randint(10,70)
-        pos_x_to_blue = (new_ball.rect[0] - blue_one.rect[0])/n
-        pos_y_to_blue = (new_ball.rect[1] - blue_one.rect[1])/n
-        pos_x_to_yellow = (new_ball.rect[0] - yellow_one.rect[0])/n
-        pos_y_to_yellow = (new_ball.rect[1] - yellow_one.rect[1])/n
-        blue_one.move_to((pos_x_to_blue, pos_y_to_blue ))
-        yellow_one.move_to((pos_x_to_yellow, pos_y_to_yellow ))
-
-def conversion_data_to_dict(input):
 # Initialize pygame
 pygame.init()
 
@@ -280,18 +174,18 @@ pygame.time.set_timer(ADDBALL, 250)
 
 
 # Instantiate player. Right now, this is just a rectangle.
-yellow_one = Player("yellow", 440, 240)
-yellow_two = Player("yellow", 390, 375)
-yellow_three = Player("yellow", 390, 95)
-yellow_four = Player("yellow", 255, 130)
-yellow_five = Player("yellow", 255, 330)
+yellow_one = Player("yellow_one", "yellow", 440, 240)
+yellow_two = Player("yellow_two","yellow", 390, 375)
+yellow_three = Player("yellow_three","yellow", 390, 95)
+yellow_four = Player("yellow_four","yellow", 255, 130)
+yellow_five = Player("yellow_five","yellow", 255, 330)
 yellow_goalkeeper = PlayerGoalKeeper("yellow",50, 240)
 
-blue_one = Player("blue", 615, 240)
-blue_two = Player("blue", 670, 100)
-blue_three = Player("blue", 670, 375)
-blue_four = Player("blue", 805, 130)
-blue_five = Player("blue", 805, 330)
+blue_one = Player("blue_one","blue", 615, 240)
+blue_two = Player("blue_two","blue", 670, 100)
+blue_three = Player("blue_three","blue", 670, 375)
+blue_four = Player("blue_four","blue", 805, 130)
+blue_five = Player("blue_five","blue", 805, 330)
 blue_goalkeeper = PlayerGoalKeeper("blue", 1005, 240)
 
 new_ball = Ball()
@@ -391,6 +285,178 @@ def reset():
 
     new_ball.add(ball)
     new_ball.unhide_ball()
+
+# def get_closest_player(team, player_w_ball):
+#     closest_player = ""
+#     tmp_distance = 0
+#     dist = 999999999999999999999999999
+#     for player in team:
+#         if player != player_w_ball:
+#             tmp_distance = distance.euclidean([player_w_ball.rect[0],player_w_ball.rect[1]],
+#                                               [player.rect[0],player.rect[1]])
+#         else:
+#             continue
+#         if tmp_distance < dist:
+#             dist = tmp_distance
+#             closest_player = player
+#
+#     return closest_player
+
+def get_random_other_player_his_team(team):
+    return random.choice(list(team))
+
+
+def handle_players_collision(player_w_ball, team_wo_ball, team_w_ball):
+    action_number = random.randint(0, 2)
+    for player_wo_ball in team_wo_ball:
+        if pygame.sprite.collide_rect(player_w_ball, player_wo_ball):
+            fg = all_data
+            if action_number == 1:
+                teammate = get_random_other_player_his_team(team_w_ball)
+                #closest_player = get_closest_player(team_w_ball, player_w_ball)
+                player_w_ball.ball = False
+                teammate.ball = True
+                teammate.has_ball()
+                player_w_ball.has_ball()
+            elif action_number == 2:
+                player_w_ball.ball = False
+                player_wo_ball.ball = True
+                player_wo_ball.has_ball()
+                player_w_ball.has_ball()
+            break
+
+def who_has_ball(all_players, yellow_team, blue_team):
+    team_wo_ball = ""
+    team_w_ball = ""
+    player_w_ball = ""
+    for player in all_players:
+        if player.ball:
+            player_w_ball = player
+
+    if player_w_ball in yellow_team:
+        team_wo_ball = blue_team
+        team_w_ball = yellow_team
+    else:
+        team_wo_ball = yellow_team
+        team_w_ball = blue_team
+
+    return player_w_ball, team_wo_ball, team_w_ball
+
+# def move_players_to_enemy_gate(b_team, y_team, b_gate, y_gate, team_ball):
+#     for player in y_team:
+#         if player in team_ball:
+#             a = "1" + (len(str(abs(player.rect[0] - b_gate.rect[0])))-1)*"0"
+#             b = "1" + (len(str(abs(player.rect[1] - b_gate.rect[1])))-1)*"0"
+#             pos_x = (b_gate.rect[0]- player.rect[0]) / (int(a)+10)
+#             pos_y = (b_gate.rect[1]- player.rect[1]) / (int(b)+10)
+#             player.move_to((pos_x, pos_y))
+#     for player in b_team:
+#         if player in team_ball:
+#             a = "1" + (len(str(abs(player.rect[0] - y_gate.rect[0])))-1)*"0"
+#             b = "1" + (len(str(abs(player.rect[1] - y_gate.rect[1])))-1)*"0"
+#             pos_x = (y_gate.rect[0]- player.rect[0]) / (int(a)+10)
+#             pos_y = (y_gate.rect[1]- player.rect[1]) / (int(b)+10)
+#             player.move_to((pos_x, pos_y))
+
+
+def check_goal(team_w_ball):
+    for player in team_w_ball:
+        if player.color == "yellow":
+            goal_area = goal_area_blue
+            goal_keeper = blue_goalkeeper
+            gate = blue_gate
+        elif player.color == "blue":
+            goal_area = goal_area_yellow
+            goal_keeper = yellow_goalkeeper
+            gate = yellow_gate
+        if pygame.sprite.collide_rect(player, goal_area) and player.ball:
+            n = random.randint(0, 1)
+            player.ball = False
+            player.has_ball()
+
+            if n == 0 and blue_gate.ball != True:
+                goal_keeper.ball = True
+                goal_keeper.has_ball()
+            elif n == 1 and goal_keeper.ball != True:
+                gate.has_ball(True)
+
+            reset()
+
+# def catch_ball_first(player_w_ball):
+#     if not player_w_ball:
+#         n = random.randint(10,70)
+#         pos_x_to_blue = (new_ball.rect[0] - blue_one.rect[0])/n
+#         pos_y_to_blue = (new_ball.rect[1] - blue_one.rect[1])/n
+#         pos_x_to_yellow = (new_ball.rect[0] - yellow_one.rect[0])/n
+#         pos_y_to_yellow = (new_ball.rect[1] - yellow_one.rect[1])/n
+#         blue_one.move_to((pos_x_to_blue, pos_y_to_blue ))
+#         yellow_one.move_to((pos_x_to_yellow, pos_y_to_yellow ))
+
+def conversion_data_to_dict(blue_team, yellow_team, ball, yellow_goalarea,
+                            blue_goalarea, blue_gate, yellow_gate,
+                            player_w_ball):
+    global all_data
+
+    player_with_ball = get_player_with_ball_to_dict(player_w_ball)
+    ball_dict = {"position_x": ball.rect[0],
+                         "position_y": ball.rect[1]}
+    blue_goalarea_dict = {"position_x": blue_goalarea.rect[0],
+                          "position_y": blue_goalarea.rect[1]}
+    yellow_goalarea_dict = {"position_x": yellow_goalarea.rect[0],
+                          "position_y": yellow_goalarea.rect[1]}
+    blue_gate_dict = {"position_x": blue_gate.rect[0],
+                       "position_y": blue_gate.rect[1],
+                       "ball": blue_gate.ball}
+    yellow_gate_dict = {"position_x": yellow_gate.rect[0],
+                       "position_y": yellow_gate.rect[1],
+                       "ball": yellow_gate.ball}
+
+    yellow_team_dict = create_dict_from_team_sprite(yellow_team)
+    blue_team_dict = create_dict_from_team_sprite(blue_team)
+
+    all_data={"yellow_team": yellow_team_dict, "blue_team": blue_team_dict,
+            "ball": ball_dict, "blue_goalarea":blue_goalarea_dict,
+            "yellow_goalarea":yellow_goalarea_dict, "blue_gate": blue_gate_dict,
+            "yellow_gate": yellow_gate_dict, "player_with_ball": player_with_ball}
+
+def get_player_with_ball_to_dict(player_w_ball):
+    player_with_ball = {}
+    if player_w_ball:
+        player_with_ball["name"] = player_w_ball.name
+        player_with_ball["color"] = player_w_ball.color
+        player_with_ball["ball"] = player_w_ball.ball
+        player_with_ball["position_x"] = player_w_ball.rect[0]
+        player_with_ball["position_y"] = player_w_ball.rect[1]
+    else:
+        player_with_ball["name"] = None
+        player_with_ball["color"] = None
+        player_with_ball["ball"] = None
+        player_with_ball["position_x"] = None
+        player_with_ball["position_y"] = None
+
+    return player_with_ball
+
+def create_dict_from_team_sprite(team):
+    team_dict = {}
+    for player in team:
+        player_dict = {}
+        id = player.name
+        player_dict["color"] = player.color
+        player_dict["ball"] = player.ball
+        player_dict["position_x"] = player.rect[0]
+        player_dict["position_y"] = player.rect[1]
+        team_dict[id] = player_dict
+
+    return team_dict
+
+
+
+def update_team_states(team, team_color):
+    for player, player_dict in zip(team, all_data[team_color]):
+        player.rect[0] = all_data[team_color][player_dict]["position_x"]
+        player.rect[1] = all_data[team_color][player_dict]["position_y"]
+        player.ball = all_data[team_color][player_dict]["ball"]
+
 # Variable to keep the main loop running
 running = True
 
@@ -400,36 +466,33 @@ port = 7976                                                             #Choosin
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)              #socket initialization
 server.bind((host, port))                                               #binding host and port to socket
 server.listen()
-first = True
 
-def handle(client, data):
-    global first
-    if first:
-        a = data
-        a = json.dumps(a)
-        a = bytes(a, 'UTF-8')
-        client[0].send(a)
-        first = False
+def handle(client):
 
-    message = client[0].recv(5000)
-
-    decoded = json.loads(message)
-    print(f"{decoded}\n")
-    a = json.dumps(decoded)
-    a = bytes(a, 'UTF-8')
-    client[1].send(a)
-
-    message = client[1].recv(5000)
-    decoded = json.loads(message)
-    print(f"{decoded}\n")
-    a = json.dumps(decoded)
+    global all_data
+    a = all_data
+    a = json.dumps(a)
     a = bytes(a, 'UTF-8')
     client[0].send(a)
+
+    x = all_data
+    message = client[0].recv(5000)
+    all_data = json.loads(message)
+    to_send = json.dumps(all_data)
+    to_send = bytes(to_send, 'UTF-8')
+    client[1].send(to_send)
+
+    message = client[1].recv(5000)
+    all_data = json.loads(message)
+
 
 clients = []
 
 
 def main():
+    ez = False
+    running = True
+    first_run = True
     # Main loop
     while running:
         # for loop through the event queue
@@ -451,21 +514,32 @@ def main():
         for entity in goal_areas:
             screen.blit(entity.surf, entity.rect)
 
+        player_w_ball, team_wo_ball, team_w_ball = who_has_ball(all_players, yellow_team, blue_team)
 
-        player_w_ball, team_wo_ball, team_w_ball = who_has_ball(all_players,yellow_team, blue_team, blue_gate)
-        catch_ball_first(player_w_ball)
+
+        conversion_data_to_dict(blue_team, yellow_team, new_ball,
+                                goal_area_yellow, goal_area_blue,
+                                blue_gate, yellow_gate, player_w_ball)
+        handle(clients)
+        update_team_states(yellow_team, "yellow_team")
+        update_team_states(blue_team, "blue_team")
+
+        if first_run:
+            conversion_data_to_dict(blue_team, yellow_team, new_ball,
+                                    goal_area_yellow, goal_area_blue,
+                                    blue_gate, yellow_gate, player_w_ball)
+            first_run = False
+
+        #catch_ball_first(player_w_ball)
         check_goal(team_w_ball)
-
         if player_w_ball:
             handle_players_collision(player_w_ball, team_wo_ball, team_w_ball)
-            closest_player = get_closest_player(team_wo_ball, player_w_ball)
-            n = random.randint(10,30)
-            pos_x = (player_w_ball.rect[0] - closest_player.rect[0])/n
-            pos_y = (player_w_ball.rect[1] - closest_player.rect[1])/n
-            closest_player.move_to((pos_x,pos_y))
-            move_players_to_enemy_gate(blue_team, yellow_team, blue_gate, yellow_gate, team_w_ball)
-
-
+            # closest_player = get_closest_player(team_wo_ball, player_w_ball)
+            # n = random.randint(10,30)
+            # pos_x = (player_w_ball.rect[0] - closest_player.rect[0])/n
+            # pos_y = (player_w_ball.rect[1] - closest_player.rect[1])/n
+            # closest_player.move_to((pos_x,pos_y))
+            # move_players_to_enemy_gate(blue_team, yellow_team, blue_gate, yellow_gate, team_w_ball)
 
         if pygame.sprite.spritecollideany(yellow_one, ball):
             # If so, then remove the player and stop the loop
@@ -474,20 +548,20 @@ def main():
             new_ball.hide_ball()
             new_ball.remove(ball)
 
+
+
         # Check if any ball have collided with the player
         if pygame.sprite.spritecollideany(blue_one, ball):
             # If so, then remove the player and stop the loop
-            yellow_one.ball = True
-            yellow_one.has_ball()
+            blue_one.ball = True
+            blue_one.has_ball()
             new_ball.hide_ball()
             new_ball.remove(ball)
 
-        # Update the display
+            # Update the display
         pygame.display.flip()
         # Ensure program maintains a rate of 30 frames per second
-        clock.tick(30)
-
-        handle(clients, data)
+        clock.tick(25)
 
 while True:
     client, address = server.accept()
@@ -495,3 +569,4 @@ while True:
     clients.append(client)
     if len(clients) == 2:
         main()
+
--- yellow_team.py
+++ yellow_team.py
@@ -1,5 +1,8 @@
 import json
-import socket, threading
+import random
+import socket
+
+from scipy.spatial import distance
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # socket initialization
 client.connect(('127.0.0.1', 7976))  # connecting client to server
@@ -8,23 +11,64 @@ class ClientHandle:
     msg = b""
     already_sent = False
 
+    def catch_ball_first(self, all_data):
+        blue_one = all_data["yellow_team"]["yellow_one"]
+        ball = all_data["ball"]
+        n = random.randint(10, 70)
+        blue_one["position_x"] = blue_one["position_x"]+(ball["position_x"] - blue_one["position_x"]) / n
+        blue_one["position_y"] = blue_one["position_y"]+(ball["position_y"] - blue_one["position_y"]) / n
 
-    def valami(self, dict_):
-        a = json.dumps(dict_)
-        a = bytes(a, 'UTF-8')
-        return a
+        return all_data
+
+    def get_closest_player_to_enemy_w_ball(self, all_data):
+        player_wb = all_data["player_with_ball"]
+        closest_player_name = "yellow_one"
+        min_distance = distance.euclidean([player_wb["position_x"], player_wb["position_y"]],
+                                          [all_data["yellow_team"][closest_player_name]["position_x"],
+                                           all_data["yellow_team"][closest_player_name]["position_y"]])
+
+        for player_name in all_data["yellow_team"]:
+            player = all_data["yellow_team"][player_name]
+            tmp_dist = distance.euclidean([player_wb["position_x"], player_wb["position_y"]],
+                                          [player["position_x"], player["position_y"]])
+            if tmp_dist < min_distance:
+                closest_player_name = player_name
+
+        return closest_player_name
+
+    def follow_ball(self, closest_player, all_data):
+        player_wb = all_data["player_with_ball"]
+        speed = random.randint(10,30)
+        all_data["yellow_team"][closest_player]["position_x"] += (player_wb["position_x"] - all_data["yellow_team"][closest_player]["position_x"]) / speed
+        all_data["yellow_team"][closest_player]["position_y"] += (player_wb["position_y"] - all_data["yellow_team"][closest_player]["position_y"]) / speed
+
+    def move_players_to_enemy_gate(self, all_data):
+        speed = (random.randint(1, 20)/10)
+        enemy_gate = all_data["blue_gate"]
+        for player in all_data["yellow_team"]:
+            dist_x = enemy_gate["position_x"]- all_data["yellow_team"][player]["position_x"]
+            dist_y = enemy_gate["position_y"]- all_data["yellow_team"][player]["position_y"]
+            a = int(("1" + (len(str(abs(dist_x)))-1)*"0"))+10
+            b = int(("1" + (len(str(abs(dist_x)))-1)*"0"))+10
+            all_data["yellow_team"][player]["position_x"] += (dist_x / a)*speed
+            all_data["yellow_team"][player]["position_x"] += (dist_y / b)*speed
 
     def run(self):
         while True:
             self.msg = client.recv(5000)
 
-            a = json.loads(self.msg)
-            print(a)
-            a = json.dumps(a)
-            a = bytes(a, 'UTF-8')
-            client.send(a)
-            self.already_sent = True
-
+            all_data = json.loads(self.msg)
+            if all_data["player_with_ball"]["name"] == None:
+                all_data = self.catch_ball_first(all_data)
+            else:
+                if all_data["player_with_ball"]["color"] == "blue":
+                    closest_player = self.get_closest_player_to_enemy_w_ball(all_data)
+                    self.follow_ball(closest_player,all_data)
+                else:
+                    self.move_players_to_enemy_gate(all_data)
+            all_data = json.dumps(all_data)
+            all_data = bytes(all_data, 'UTF-8')
+            client.send(all_data)
 
 c_hand = ClientHandle()
 c_hand.run()
